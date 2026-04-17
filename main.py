2b495de50080b23fd577803cdc0e8e12caba0ddb
sethespinoza <seth.espinoza8@gmail.com>
2026-04-16 21:39:08 -0500

    added database & models - users & workout tables

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from database import engine
+from models import Base
+
+# reads models and create tables in db
+Base.metadata.create_all(bind=engine)
+
 
 # app instance
 # all requests from the server are handled through this object
@@ -11,13 +17,7 @@ def home():
     return {"message": "LiftedUp API is running!"}
 
 # dynamic route with path parameter
-# the {user_id} in the URL is a placeholder for a dynamic value
-# the function get_user will receive the user_id as an argument
-# when a request is made to /users/123, the get_user function will be called with user_id=123
-# the function returns a JSON response with user details
-# in a real application, you would fetch this data from a database instead of hardcoding it
-# the response includes the user's id, username, and their personal records for flat bench and hack squat
-#
+
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     return {
